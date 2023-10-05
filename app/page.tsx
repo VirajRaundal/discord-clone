19ace05d4b3aec204a10747eaa540d02bd2b1b06
Viraj Raundal <72504150+VirajRaundal@users.noreply.github.com>
2023-10-06 00:26:06 +0530

    Changed font

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image'
+import { Button } from "@/components/ui/button"
+
 
 export default function Home() {
   return(
-    <div className='text-3xl font-bold text-indigo-500'>
-      Hello!
+    <div className=' text-3xl font-bold text-indigo-500'>
+      <p>Hello!</p>
+      <Button variant="outline" >Click Me</Button>
     </div>
   )
 }
