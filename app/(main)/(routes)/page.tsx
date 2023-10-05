bf3d1d6cf8d53bd80e395ac325a0a9b5f07f31e7
Viraj Raundal <72504150+VirajRaundal@users.noreply.github.com>
2023-10-06 00:45:08 +0530

    File structure and login and register page basic

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -4,8 +4,7 @@ import { Button } from "@/components/ui/button"
 export default function Home() {
   return(
     <div className=' text-3xl font-bold text-indigo-500'>
-      <p>Hello!</p>
-      <Button variant="outline" >Click Me</Button>
+      This is a protected route.
     </div>
   )
 }
