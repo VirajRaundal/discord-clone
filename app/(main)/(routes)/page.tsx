587fd770e2c950238c62d87789dd5f042c9d4bdf
Viraj Raundal <72504150+VirajRaundal@users.noreply.github.com>
2023-10-06 01:34:26 +0530

    Auth complete

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@/components/ui/button"
+import { UserButton } from "@clerk/nextjs/app-beta"
 
 
 export default function Home() {
   return(
     <div className=' text-3xl font-bold text-indigo-500'>
-      This is a protected route.
+      <UserButton afterSignOutUrl="/"/>
     </div>
   )
 }
