d3590a2e71bc96c01687692527eb08693b5bd540
Viraj Raundal <72504150+VirajRaundal@users.noreply.github.com>
2023-10-09 21:08:04 +0530

    Added components for intial modal UI

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,5 +1,26 @@
+import { initialProfile } from "@/lib/initial-profile"
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+
 const SetupPage = async() => {
-    return <div>Create a server!</div>
+
+    const profile = await initialProfile();
+
+    const server = await db.server.findFirst({
+        where: {
+            members: {
+                some:{
+                    profileId: profile.id
+                }
+            }
+        }
+    })
+
+    if(server){
+        return redirect(`/server/${server.id}`)
+    }
+
+    return <InitialModal />
 }
 
 export default SetupPage
